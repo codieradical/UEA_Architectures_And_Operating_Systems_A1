a31b81bb25a638553a1f3ec1170df362150d027c
Alex <git@alexnewark.co.uk>
2018-11-13 14:40:45 +0000

    Fill message array.

--- cipher.c
+++ cipher.c
@@ -14,15 +14,42 @@ Author      : Alex H. Newark
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 static char privateKey[104];
-static char rowSize;
+static char rowSize, columnSize;
+static short messageLength;
+static char message[1000];
+static char order[104];
 
-enum executionModeFlags {
-    Encrypting = 0, 
-    Decrypting = 1
-};
+void printMessage() {
+    
+    printf("\n%s\n\n", privateKey);
+
+    for(short i = 0; i < messageLength; i++)
+    {
+        printf("%c", message[i]);
+
+        if((i + 1) % rowSize == 0) {
+            printf("\n");
+        }
+    }
+
+    printf("\n\n");
+}
+
+void encrypt() {
+
+    printMessage();
+
+    // for(char i = 0; i < rowSize; i++) {
+    //     char
+    // }
+    
+}
+
+void decrypt() {
+    printMessage();
+}
 
 int main(int argc, char *argv[]) 
 {
@@ -31,12 +58,7 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    unsigned int executionMode = Encrypting;
-
-    if (sscanf(argv[1], "%i", &executionMode) != 1 | executionMode > 1) {
-        printf("Error: Unknown execution mode. Expected 0 or 1, got \"%s\".\n", argv[1]);
-        return EXIT_FAILURE;
-    }
+    char encrypting = argv[1][0] - 48;
 
     // The primary key could be up to 1000 characters long, but that'd be overkill.
     
@@ -48,18 +70,18 @@ int main(int argc, char *argv[])
 
         if(claPK[claIndex] == 0) {
             if(pkIndex < 3) {
-                printf("Error: Private key is too short. Must be more than 2 characters.");
+                printf("Error: Private key is too short. Must be more than 2 characters.\n");
                 return EXIT_FAILURE;
             }
             parsingPK = 0;
         }
 
         if(pkIndex > 103) {
-            printf("Error: Private key is too long. Must be less than 105 characters.");
+            printf("Error: Private key is too long. Must be less than 105 characters.\n");
             return EXIT_FAILURE;
         }
 
-        if(pkChar >  64 && pkChar < 91) {
+        if(pkChar > 64 && pkChar < 91) {
             pkChar = pkChar + 32;
         }
 
@@ -70,11 +92,51 @@ int main(int argc, char *argv[])
 
         claIndex++;
     }
+
+    rowSize = pkIndex;
+    
+    short messageIndex = 0;
+    char parsingMessage = 1;
+
+    while(parsingMessage) {
+        char messageChar = getchar();
+
+        if(messageIndex > 999) {
+            printf("Error: Message is too long. Must be less than or equal to 1000 characters.");
+            return EXIT_FAILURE;
+        }
+
+        if(messageChar == -1) {
+            if(messageIndex < 2) {
+                printf("Error: Please provide a message.\n");
+                return EXIT_FAILURE;
+            }
+            parsingMessage = 0;
+        }
+
+        if(messageChar > 64 && messageChar < 91) {
+            messageChar = messageChar + 32;
+        }
+
+        if(messageChar > 96 && messageChar < 123) {
+            message[messageIndex] = messageChar;
+            messageIndex++;
+        }
+    }
+
+    messageLength = messageIndex;
+    columnSize = (messageLength / rowSize) + 1;
     
 
-    printf("%s", privateKey);
+    if(encrypting) {
+        encrypt();
+    } else {
+        decrypt();
+    }
+
     
     return EXIT_SUCCESS;
 }
+
 
 /**********************************************************************/
