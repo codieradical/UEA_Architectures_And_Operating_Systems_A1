2332435b28958c31200741d21abc01f0486b64e5
Alex Newark <git@alexnewark.co.uk>
2018-11-13 23:00:28 +0000

    Load argument 1.

--- cipher.s
+++ cipher.s
@@ -12,6 +12,15 @@ Author      : Alex H. Newark
 
 ***********************************************************************/
 
+/************************************************************************
+
+REGISTERS:
+r0: General temporary storage.
+r11: argv pointer
+r12: Executition Mode (0 = encrypting, 1 = decrypting)
+
+***********************************************************************/
+
 .data @ code section starts here
 .balign 1
 sorted_primary_key: .skip 104
@@ -22,12 +31,33 @@ row_count: .skip 2
 message_length: .skip 2
 .balign 1
 message: .skip 1000
+.balign 4
+test_print: .ascii "test: %c\n"
 
 .text
 .global main
 main:
-    PUSH r4, r12
-    MOV r1, #3      @move immediate into r1
-    ADDS r0,r1,#2   @r0=r1+immediate
-    BX lr           @ return summation result to OS
+    PUSH {r4, r12}
+    PUSH {lr}
+
+    MOV r11, r1
+
+    LDR r0, =test_print
+    LDR r12,[r11, #4] 	@Move address of argv to r0
+    @ADDS r1, r0, #4	@Move to second argument (execution mode)
+    LDRSB r12, [r12]	@Move first character of second string to r1.
+    SUBS r12, r12, #44
+    MOV r1, r12
+    @MOV r12, r1		@Move r1 to r12 for permenant storage.
+    @MOV r0, r1
+    @ADDS r0, r0, #4	@move r0 to the third argument (private key)
+    BL printf
+
+    
+    @LDR r1, [r11, #8]
+    
+
+    POP {lr}
+    POP {r4, r12}
+    BX lr
     
